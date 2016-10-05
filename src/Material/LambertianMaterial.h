7f7aaa82b29fd16c6191fc7c44bbea20d7edad28
frepr183@student.liu.se <frepr183@student.liu.se>
2016-10-05 20:25:01 +0200

    Lambertian material test with Github indentation. Signed-off-by: frepr183@student.liu.se <frepr183@student.liu.se>

--- src/Material/LambertianMaterial.h
+++ src/Material/LambertianMaterial.h
@@ -4,5 +4,6 @@
 class LambertianMaterial : public Material {
 public:
 	glm::vec3 CalculateBRDF(glm::vec3& inDirection, glm::vec3& outDirection,
-							glm::vec3& normal, glm::vec3 & incomingIntensity, glm::vec3 & surfaceColor);
+							glm::vec3& normal, glm::vec3 & incomingIntensity,
+							glm::vec3 & surfaceColor);
 };
