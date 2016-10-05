ec6876d1967aaa28bd54dcee98505e430b868c93
davli538 <davli538@student.liu.se>
2016-10-05 18:50:12 +0200

    added material abstract class and lambertian material with brdf
    Signed-off-by: davli538 <davli538@student.liu.se>

---
+++ src/Material/LambertianMaterial.cpp
@@ -0,0 +1,8 @@
+#include "LambertianMaterial.h"
+
+glm::vec3 LambertianMaterial::CalculateBRDF(glm::vec3 & inDirection, glm::vec3 & outDirection, glm::vec3 & normal, glm::vec3 incomingIntensity, glm::vec3 surfaceColor)
+{
+	float dotProduct = glm::max(0.0f, glm::dot(inDirection, normal));
+	return dotProduct*incomingIntensity*surfaceColor;
+}
+
