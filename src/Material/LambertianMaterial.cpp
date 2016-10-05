6ada48004085c0c999131e31c2d7c40a86bdb031
frepr183@student.liu.se <frepr183@student.liu.se>
2016-10-05 20:26:56 +0200

    Lambertian material test with Github indentation. Signed-off-by: frepr183@student.liu.se <frepr183@student.liu.se>

--- src/Material/LambertianMaterial.cpp
+++ src/Material/LambertianMaterial.cpp
@@ -3,9 +3,10 @@
 glm::vec3 LambertianMaterial::CalculateBRDF(glm::vec3 & inDirection, glm::vec3 & outDirection,
 											glm::vec3 & normal, glm::vec3 & incomingIntensity,
 											glm::vec3 & surfaceColor) {
-	float dotProduct = glm::max(0.0f, glm::dot(inDirection, normal));
-	return dotProduct * glm::vec3(incomingIntensity.x * surfaceColor.x,
-								  incomingIntensity.y * surfaceColor.y,
-								  incomingIntensity.z * surfaceColor.z);
+	float d = glm::max(0.0f, glm::dot(inDirection, normal));
+	float r = incomingIntensity.x * surfaceColor.x;
+	float g = incomingIntensity.y * surfaceColor.y;
+	float b = incomingIntensity.z * surfaceColor.z;
+	return d * glm::vec3(r, g, b);
 }
 
