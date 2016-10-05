8b46f9ac471311aa6df2e660c9ecd763ac82164c
davli538 <davli538@student.liu.se>
2016-10-05 19:30:12 +0200

    added light intensity to material
    Signed-off-by: davli538 <davli538@student.liu.se>

--- src/Material/LambertianMaterial.cpp
+++ src/Material/LambertianMaterial.cpp
@@ -4,7 +4,6 @@ glm::vec3 LambertianMaterial::CalculateBRDF(glm::vec3 & inDirection, glm::vec3 &
 											glm::vec3 & normal, glm::vec3 & incomingIntensity,
 											glm::vec3 & surfaceColor) {
 	float dotProduct = glm::max(0.0f, glm::dot(inDirection, normal));
-
 	return dotProduct * glm::vec3(incomingIntensity.x * surfaceColor.x,
 								  incomingIntensity.y * surfaceColor.y,
 								  incomingIntensity.z * surfaceColor.z);
