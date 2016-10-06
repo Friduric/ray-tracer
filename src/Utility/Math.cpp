b84c431fc82c225964f8e7156de52c0e7ddf8a3f
frepr183@student.liu.se <frepr183@student.liu.se>
2016-10-06 15:38:21 +0200

    Quad interpolation fix. Does it wurk nuw? Signed-off-by: frepr183@student.liu.se <frepr183@student.liu.se>

--- src/Utility/Math.cpp
+++ src/Utility/Math.cpp
@@ -10,15 +10,18 @@ float Math::InterpolationQuad4f(const float dy, const float dz,
 	assert(dz >= 0 && dz <= 1);
 	const float idy = 1 - dy;
 	const float idz = 1 - dz;
-	const float isqrt2 = 1.0f / sqrt(2.0f);
-	const float dist1 = sqrt(idy * idy + dz * dz) * isqrt2; // Distance to "lower right" point.
-	const float dist2 = sqrt(dy * dy + dz * dz) * isqrt2; // Distance to "lower left" point.
-	const float dist3 = sqrt(dy * dy + idz * idz) * isqrt2; // Distance to "upper left" point.
-	const float dist4 = sqrt(idy * idy + idz * idz) * isqrt2; // Distance to "upper right" point.
+	float dist1 = sqrt(idy * idy + dz * dz); // Distance to "lower right" point.
+	float dist2 = sqrt(dy * dy + dz * dz); // Distance to "lower left" point.
+	float dist3 = sqrt(dy * dy + idz * idz); // Distance to "upper left" point.
+	float dist4 = sqrt(idy * idy + idz * idz); // Distance to "upper right" point.
+	float distsum = dist1 + dist2 + dist3 + dist4;
+	dist1 /= distsum;
+	dist2 /= distsum;
+	dist3 /= distsum;
+	dist4 /= distsum;
 	assert(dist1 >= 0 && dist1 <= 1 + FLT_EPSILON);
 	assert(dist2 >= 0 && dist2 <= 1 + FLT_EPSILON);
 	assert(dist3 >= 0 && dist3 <= 1 + FLT_EPSILON);
 	assert(dist4 >= 0 && dist4 <= 1 + FLT_EPSILON);
-	float res = (1.0f - dist1) * x1 + (1.0f - dist2) * x2 + (1.0f - dist3) * x3 + (1.0f - dist4) * x4;
-	return 0.5f * res;
+	return (1.0f - dist1) * x1 + (1.0f - dist2) * x2 + (1.0f - dist3) * x3 + (1.0f - dist4) * x4;
 }
