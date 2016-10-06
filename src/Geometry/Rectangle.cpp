8ed3ac5236b9a48e27370f53519ccc784170cb6c
frepr183@student.liu.se <frepr183@student.liu.se>
2016-10-06 14:03:16 +0200

    Rectangle class. Currently unused. Signed-off-by: frepr183@student.liu.se <frepr183@student.liu.se>

---
+++ src/Geometry/Rectangle.cpp
@@ -0,0 +1,4 @@
+#include "Rectangle.h"
+
+Rectangle::Rectangle(float _x, float _y, float _width, float _height) :
+	x(_x), y(_y), width(_width), height(_height) {}
---
+++ src/Geometry/Rectangle.h
@@ -0,0 +1,8 @@
+#pragma once
+
+class Rectangle {
+public:
+	float x, y;
+	float width, height;
+	Rectangle(float x, float y, float width, float height);
+};
