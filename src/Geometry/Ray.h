c97d065e2048bc72ebaf29a6ce13224435536b8d
frepr183@student.liu.se <frepr183@student.liu.se>
2016-10-06 11:18:53 +0200

    Ray needs a from and to vector. Signed-off-by: frepr183@student.liu.se <frepr183@student.liu.se>

--- src/Geometry/Ray.h
+++ src/Geometry/Ray.h
@@ -5,5 +5,5 @@
 class Ray {
 public:
 	glm::vec3 color, dir, from, to;
-	Ray(glm::vec3 from = glm::vec3(), glm::vec3 to = glm::vec3(), glm::vec3 color = glm::vec3());
+	Ray(glm::vec3 from, glm::vec3 to, glm::vec3 color = glm::vec3(0, 0, 0));
 };
