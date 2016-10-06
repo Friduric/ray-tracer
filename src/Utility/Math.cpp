9dbc9e06158ad854ea5f465c3fccaf3460ad3efe
frepr183@student.liu.se <frepr183@student.liu.se>
2016-10-06 16:00:02 +0200

    Alpha blend in quad interpolation instead of dist blend. Signed-off-by: frepr183@student.liu.se <frepr183@student.liu.se>

--- src/Utility/Math.cpp
+++ src/Utility/Math.cpp
@@ -10,19 +10,19 @@ float Math::InterpolationQuad4f(const float dy, const float dz,
 	assert(dz >= 0 && dz <= 1);
 	const float idy = 1 - dy;
 	const float idz = 1 - dz;
-	float dist1 = sqrt(idy * idy + dz * dz); // Distance to "lower right" point.
-	float dist2 = sqrt(dy * dy + dz * dz); // Distance to "lower left" point.
-	float dist3 = sqrt(dy * dy + idz * idz); // Distance to "upper left" point.
-	float dist4 = sqrt(idy * idy + idz * idz); // Distance to "upper right" point.
-	float distsum = dist1 + dist2 + dist3 + dist4;
-	dist1 /= distsum;
-	dist2 /= distsum;
-	dist3 /= distsum;
-	dist4 /= distsum;
-	assert(dist1 >= 0 && dist1 <= 1 + FLT_EPSILON);
-	assert(dist2 >= 0 && dist2 <= 1 + FLT_EPSILON);
-	assert(dist3 >= 0 && dist3 <= 1 + FLT_EPSILON);
-	assert(dist4 >= 0 && dist4 <= 1 + FLT_EPSILON);
-	assert(dist1 + dist2 + dist3 + dist4 >= 1 - FLT_EPSILON && dist1 + dist2 + dist3 + dist4 <= 1 + FLT_EPSILON);
-	return (1.0f - dist1) * x1 + (1.0f - dist2) * x2 + (1.0f - dist3) * x3 + (1.0f - dist4) * x4;
+	float alpha1 = 1.0f - sqrt(idy * idy + dz * dz); // Distance to "lower right" point.
+	float alpha2 = 1.0f - sqrt(dy * dy + dz * dz); // Distance to "lower left" point.
+	float alpha3 = 1.0f - sqrt(dy * dy + idz * idz); // Distance to "upper left" point.
+	float alpha4 = 1.0f - sqrt(idy * idy + idz * idz); // Distance to "upper right" point.
+	float alphaSum = alpha1 + alpha2 + alpha3 + alpha4;
+	alpha1 /= alphaSum;
+	alpha2 /= alphaSum;
+	alpha3 /= alphaSum;
+	alpha4 /= alphaSum;
+	assert(alpha1 >= 0 && alpha1 <= 1 + FLT_EPSILON);
+	assert(alpha2 >= 0 && alpha2 <= 1 + FLT_EPSILON);
+	assert(alpha3 >= 0 && alpha3 <= 1 + FLT_EPSILON);
+	assert(alpha4 >= 0 && alpha4 <= 1 + FLT_EPSILON);
+	assert(alpha1 + alpha2 + alpha3 + alpha4 >= 1 - FLT_EPSILON && alpha1 + alpha2 + alpha3 + alpha4 <= 1 + FLT_EPSILON);
+	return alpha1 * x1 + alpha2 * x2 + alpha3 * x3 + alpha4 * x4;
 }
