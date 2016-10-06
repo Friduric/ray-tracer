e3327c6b6d32dae5074377d2f920f21742c980fd
frepr183@student.liu.se <frepr183@student.liu.se>
2016-10-06 14:55:02 +0200

    Constified some arguments to quad interpolation. Signed-off-by: frepr183@student.liu.se <frepr183@student.liu.se>

--- src/Utility/Math.cpp
+++ src/Utility/Math.cpp
@@ -3,16 +3,18 @@
 #include <algorithm>
 #include <cassert>
 
-float Math::InterpolationQuad4f(float dy, float dz, float x1, float x2, float x3, float x4) {
+float Math::InterpolationQuad4f(const float dy, const float dz,
+								const float x1, const float x2,
+								const float x3, const float x4) {
 	assert(dy >= 0 && dy <= 1);
 	assert(dz >= 0 && dz <= 1);
-	float idy = 1 - dy;
-	float idz = 1 - dz;
+	const float idy = 1 - dy;
+	const float idz = 1 - dz;
 	const float isqrt2 = 1.0f / sqrt(2.0f);
-	float dist1 = sqrt(idy * idy + dz * dz) * isqrt2; // Distance to "lower right" point.
-	float dist2 = sqrt(dy * dy + dz * dz) * isqrt2; // Distance to "lower left" point.
-	float dist3 = sqrt(dy * dy + idz * idz) * isqrt2; // Distance to "upper left" point.
-	float dist4 = sqrt(idy * idy + idz * idz) * isqrt2; // Distance to "upper right" point.
+	const float dist1 = sqrt(idy * idy + dz * dz) * isqrt2; // Distance to "lower right" point.
+	const float dist2 = sqrt(dy * dy + dz * dz) * isqrt2; // Distance to "lower left" point.
+	const float dist3 = sqrt(dy * dy + idz * idz) * isqrt2; // Distance to "upper left" point.
+	const float dist4 = sqrt(idy * idy + idz * idz) * isqrt2; // Distance to "upper right" point.
 	assert(dist1 >= 0 && dist1 <= 1 + FLT_EPSILON);
 	assert(dist2 >= 0 && dist2 <= 1 + FLT_EPSILON);
 	assert(dist3 >= 0 && dist3 <= 1 + FLT_EPSILON);
