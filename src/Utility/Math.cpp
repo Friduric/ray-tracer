73229f4a4da6d2902917e0a86fde6d8cfe36e570
frepr183@student.liu.se <frepr183@student.liu.se>
2016-10-06 14:03:33 +0200

    Definition/declaration of quad interpolation using 4f. Signed-off-by: frepr183@student.liu.se <frepr183@student.liu.se>

---
+++ src/Utility/Math.cpp
@@ -0,0 +1,6 @@
+#include "Math.h"
+
+float Math::InterpolateQuad4f(float dy, float dz, float x1, float x2, float x3, float c4)
+{
+	return 0;
+}
---
+++ src/Utility/Math.h
@@ -0,0 +1,5 @@
+#pragma once
+
+namespace Math {
+	static float InterpolateQuad4f(float dy, float dz, float x1, float x2, float x3, float c4);
+}
