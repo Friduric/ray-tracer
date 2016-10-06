a21c9910b274acd33eb4fda1aeca3831f64a8c21
frepr183@student.liu.se <frepr183@student.liu.se>
2016-10-06 20:42:54 +0200

    Removed rectangle class. Signed-off-by: frepr183@student.liu.se <frepr183@student.liu.se>

--- src/Geometry/Rectangle.cpp
+++
@@ -1,4 +0,0 @@
-#include "Rectangle.h"
-
-Rectangle::Rectangle(float _x, float _y, float _width, float _height) :
-	x(_x), y(_y), width(_width), height(_height) {}
--- src/Geometry/Rectangle.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-class Rectangle {
-public:
-	float x, y;
-	float width, height;
-	Rectangle(float x, float y, float width, float height);
-};
