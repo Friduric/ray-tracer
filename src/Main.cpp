605a4d7afdf976507432e46c123fcf887c94abff
frepr183@student.liu.se <frepr183@student.liu.se>
2016-10-07 10:33:28 +0200

    Cleaned main a bit. Signed-off-by: frepr183@student.liu.se <frepr183@student.liu.se>

--- src/Main.cpp
+++ src/Main.cpp
@@ -11,22 +11,18 @@ int main()
 {
 	std::cout << "Starting..." << std::endl;
 
-	// --- Interp test ---
-	/*float t = Math::InterpolationQuad4f(0, 0, 0, 0, 1, 1);
-	cout << t << endl;
-	t = Math::InterpolationQuad4f(1, 1, 0, 0, 1, 1);
-	cout << t << endl;*/
-	// --- body ---
+	/* Initialize camera. */
 	Camera c(90, 90);
+
+	/* Create scene. */
 	Scene scene;
 	scene.CreateRoom();
 	scene.CreateSphere(10, 0, 2, 1.0f);
 	scene.CreateTetrahedron(3, 0, 0);
-	//			c1					c2					c3					c4
-	// c.Render(scene, glm::vec3(-2, 0, 0), glm::vec3(0, 1, -1), glm::vec3(0, -1, -1), glm::vec3(0, -1, 1), glm::vec3(0, 1, 1), 1000000.0f, 1);
+
+	/* Render. */
 	c.Render(scene);
 	c.WriteImageToTGA();
-	// ------------
 
 	std::cout << "Exiting... press any key to exit." << std::endl;
 	std::cin.get();
