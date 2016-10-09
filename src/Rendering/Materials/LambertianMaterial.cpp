e17abb40fcb823a85d3d1fe57e18dad20ec42bb1
frepr183@student.liu.se <frepr183@student.liu.se>
2016-10-09 23:25:56 +0200

    Doing surface and radiance gamma testing on Lambertian materials. Signed-off-by: frepr183@student.liu.se <frepr183@student.liu.se>

--- src/Rendering/Materials/LambertianMaterial.cpp
+++ src/Rendering/Materials/LambertianMaterial.cpp
@@ -21,9 +21,12 @@ glm::vec3 LambertianMaterial::CalculateBRDF(const glm::vec3 & inDirection,
 											const glm::vec3 & normal,
 											const glm::vec3 & incomingRadiance) const {
 	const float d = glm::max(0.0f, glm::dot(inDirection, normal));
-	const float r = incomingRadiance.r * surfaceColor.r;
-	const float g = incomingRadiance.g * surfaceColor.g;
-	const float b = incomingRadiance.b * surfaceColor.b;
+	constexpr float radianceGamma = 0.002f;
+	constexpr float surfaceGamma = 0.001f;
+	const float l = glm::length(incomingRadiance);
+	const float r = incomingRadiance.r * surfaceColor.r + incomingRadiance.r * radianceGamma + surfaceColor.r * l * surfaceGamma;
+	const float g = incomingRadiance.g * surfaceColor.g + incomingRadiance.g * radianceGamma + surfaceColor.g * l * surfaceGamma;
+	const float b = incomingRadiance.b * surfaceColor.b + incomingRadiance.b * radianceGamma + surfaceColor.b * l * surfaceGamma;
 	return reflectance * d * glm::vec3(r, g, b);
 }
 
