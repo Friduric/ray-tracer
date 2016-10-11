86186a9b0cac1f7858294f8b609bd7fc6d14128a
frepr183@student.liu.se <frepr183@student.liu.se>
2016-10-11 11:05:59 +0200

    Adjusted photon map code length. A few optimizations too. Signed-off-by: frepr183@student.liu.se <frepr183@student.liu.se>

--- src/PhotonMap/PhotonMap.cpp
+++ src/PhotonMap/PhotonMap.cpp
@@ -3,77 +3,57 @@
 #include "PhotonMap.h"
 #include "../Utility/Math.h"
 
-void PhotonMap::CreatePhotonMap(const Scene & scene, const unsigned int photonsPerLightSource,
-								const unsigned int maxPhotonsPerNode, const float minSizeOfNodeBox,
+void PhotonMap::CreatePhotonMap(const Scene & scene, const unsigned int PHOTONS_PER_LIGHT_SOURCE,
+								const unsigned int MAX_PHOTONS_PER_NODE,
+								const float MINIMUM_NODE_BOX_DIMENSION,
 								const unsigned int MAX_DEPTH) {
 
-	// Initialize.
-	std::uniform_int_distribution<int> integerDistribution;
-	std::default_random_engine generator;
-	int primIdx;
+	/* Initialize. */
 	std::cout << "Creating the photon map ..." << std::endl;
 	Ray ray;
 
-	// For each light source.
+	/* For each light source. */
 	for (unsigned int i = 0; i < scene.emissiveRenderGroups.size(); ++i) {
-		const auto & lightSource = *scene.emissiveRenderGroups[i];
-		primIdx = rand() % lightSource.primitives.size();
-		const auto & prim = *lightSource.primitives[primIdx];
-		for (unsigned int j = 0; j < photonsPerLightSource; ++j) {
 
-			// Shoot photon in a random direction.
-			glm::vec3 surfPos = prim.GetRandomPositionOnSurface();
-			glm::vec3 normal = prim.GetNormal(surfPos);
-			glm::vec3 dir = Math::CosineWeightedHemisphereSampleDirection(normal);
+		/* Fetch data about current light source. */
+		const auto * lightSource = scene.emissiveRenderGroups[i];
+		const auto * prim = lightSource->primitives[rand() % lightSource->primitives.size()];
 
-			ray.from = surfPos;
-			ray.dir = dir;
+		for (unsigned int j = 0; j < PHOTONS_PER_LIGHT_SOURCE; ++j) {
 
-			unsigned int intersectionRenderGroupIdx;
-			unsigned int intersectionPrimitiveIdx;
+			/* Shoot photon in a random direction. */
+			ray.from = prim->GetRandomPositionOnSurface();
+			glm::vec3 normal = prim->GetNormal(ray.from);
+			ray.dir = Math::CosineWeightedHemisphereSampleDirection(normal);
+			glm::vec3 photonRadiance = lightSource->material->GetEmissionColor();
+
+			unsigned int intersectionRenderGroupIdx, intersectionPrimitiveIdx;
 			float intersectionDistance;
+			for (unsigned int k = 0; k < MAX_DEPTH; ++k) {
+				if (scene.RayCast(ray, intersectionRenderGroupIdx, intersectionPrimitiveIdx, intersectionDistance)) {
 
-			if (scene.RayCast(ray, intersectionRenderGroupIdx, intersectionPrimitiveIdx, intersectionDistance)) {
-
-				// Save photon in the octree.
-				glm::vec3 hitSurfPos = ray.from + intersectionDistance * ray.dir;
-				Primitive & prim = *(scene.renderGroups[intersectionRenderGroupIdx].primitives[intersectionPrimitiveIdx]);
-				Material* material = scene.renderGroups[intersectionRenderGroupIdx].material;
-				glm::vec3 norm = prim.GetNormal(hitSurfPos);
-				glm::vec3 refl = glm::reflect(dir, norm);
-				glm::vec3 color = material->CalculateDiffuseLighting(-dir, refl, norm, lightSource.material->GetEmissionColor());
-				octree.photons.emplace_back(hitSurfPos, dir, color);
-
-				// Trace each photon in scene.
-				for (unsigned int k = 0; k < MAX_DEPTH; ++k) {
-					ray.dir = refl;
-					ray.from = hitSurfPos;
-					dir = refl;
-					surfPos = hitSurfPos;
-					if (scene.RayCast(ray, intersectionRenderGroupIdx, intersectionPrimitiveIdx, intersectionDistance)) {
-						// Save photon in the octree
-						hitSurfPos = surfPos + intersectionDistance * dir;
-						Primitive & otherPrim = *(scene.renderGroups[intersectionRenderGroupIdx].primitives[intersectionPrimitiveIdx]);
-						material = scene.renderGroups[intersectionRenderGroupIdx].material;
-						norm = otherPrim.GetNormal(hitSurfPos);
-						refl = glm::reflect(dir, norm);
-						color = material->CalculateDiffuseLighting(dir, refl, norm, color);
-						octree.photons.emplace_back(hitSurfPos, dir, color);
-					}
-					else {
-						// Break if we did not hit any more surface.
-						break;
-					}
+					// Save photon in the octree.
+					glm::vec3 intersectionPosition = ray.from + intersectionDistance * ray.dir;
+					Primitive * prim = scene.renderGroups[intersectionRenderGroupIdx].primitives[intersectionPrimitiveIdx];
+					Material * material = scene.renderGroups[intersectionRenderGroupIdx].material;
+					glm::vec3 intersectionNormal = prim->GetNormal(intersectionPosition);
+					glm::vec3 rayReflection = glm::reflect(ray.dir, intersectionNormal);
+					photonRadiance = material->CalculateDiffuseLighting(ray.dir, rayReflection, intersectionNormal, photonRadiance);
+					octree.photons.push_back(Photon(intersectionPosition, ray.dir, photonRadiance));
+					ray.from = intersectionPosition;
+					ray.dir = rayReflection;
+				}
+				else {
+					break;
 				}
 			}
-
 		}
 	}
 
 	// Set Up the octree.
-	octree.SetUpOctree(scene, maxPhotonsPerNode, minSizeOfNodeBox);
+	octree.SetUpOctree(scene, MAX_PHOTONS_PER_NODE, MINIMUM_NODE_BOX_DIMENSION);
 }
 
-std::vector<Photon*> & PhotonMap::GetPhotonsAtPosition(const glm::vec3 pos) {
+std::vector<Photon*> & PhotonMap::GetPhotonsAtPosition(const glm::vec3 & pos) {
 	return octree.GetNodeClosestToPosition(pos)->photons;
 }
--- src/PhotonMap/PhotonMap.h
+++ src/PhotonMap/PhotonMap.h
@@ -10,21 +10,21 @@ public:
 	/// Shoots all photons in the scene and saves them in a Octree.
 	/// </summary>
 	/// <param name='scene'> The scene. </param>
-	/// <param name='photonsPerLightSource'> The amount of photons used per light source. </param>
-	/// <param name='maxPhotonsPerNode'> The maximum amount of photons per node. </param>
-	/// <param name='minSizeOfNodeBox'> The minimum width, height and depth of a nodes box size. </param>
+	/// <param name='PHOTONS_PER_LIGHT_SOURCE'> The amount of photons used per light source. </param>
+	/// <param name='MAX_PHOTONS_PER_NODE'> The maximum amount of photons per node. </param>
+	/// <param name='MINIMUM_NODE_BOX_DIMENSION'> The minimum width, height and depth of a nodes box size. </param>
 	/// <param name='MAX_DEPTH'> The amount of times each photon will bounce at most. </param>
 	void CreatePhotonMap(const Scene & scene,
-						 const unsigned int photonsPerLightSource,
-						 const unsigned int maxPhotonsPerNode,
-						 const float minSizeOfNodeBox,
+						 const unsigned int PHOTONS_PER_LIGHT_SOURCE,
+						 const unsigned int MAX_PHOTONS_PER_NODE,
+						 const float MINIMUM_NODE_BOX_DIMENSION,
 						 const unsigned int MAX_DEPTH);
 
 	/// <summary> 
 	/// Returns all photons located at the given world position.
 	/// </summary>
 	/// <param name='pos'> The position to search around. </param>
-	std::vector<Photon*> & GetPhotonsAtPosition(const glm::vec3 pos);
+	std::vector<Photon*> & GetPhotonsAtPosition(const glm::vec3 & pos);
 
 private:
 
