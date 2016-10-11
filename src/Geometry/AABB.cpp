4581a6b3d824a2a770ae36c34dd98716b68b8027
frepr183@student.liu.se <frepr183@student.liu.se>
2016-10-11 14:13:47 +0200

    Fixed slight mistake in AABB.cpp. Signed-off-by: frepr183@student.liu.se <frepr183@student.liu.se>

--- src/Geometry/AABB.cpp
+++ src/Geometry/AABB.cpp
@@ -6,7 +6,7 @@ bool AABB::IsPointInsideAABB(const glm::vec3 & p) const {
 	if (p.x < minimum.x || p.y < minimum.y || p.z < minimum.z) {
 		return false;
 	}
-	if (p.x > maximum.x || p.y > minimum.y || p.z > maximum.z) {
+	if (p.x > maximum.x || p.y > maximum.y || p.z > maximum.z) {
 		return false;
 	}
 	return true;
