fe4c0e67500c51ffb86f2e891c1bf90bf054693b
frepr183@student.liu.se <frepr183@student.liu.se>
2016-10-11 14:17:29 +0200

    Added another constructor to the AABB class. Signed-off-by: frepr183@student.liu.se <frepr183@student.liu.se>

--- src/Geometry/AABB.h
+++ src/Geometry/AABB.h
@@ -9,6 +9,10 @@ public:
 	/// <summary> Constructs an AABB on the form (minimum, maximum). </summary>
 	AABB(const glm::vec3 minimum, const glm::vec3 maximum);
 
+	/// <summary> Constructs an AABB on the form ({minX, minY, minZ}, {maxX, maxY, maxZ}). </summary>
+	AABB(const float minX, const float minY, const float minZ,
+		 const float maxX, const float maxY, const float maxZ);
+
 	/// <summary> Returns true if the given point is inside this AABB. </summary>
 	bool IsPointInsideAABB(const glm::vec3 & point) const;
 };
