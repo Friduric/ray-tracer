61adf292fdf1ca1662207ff6616f8c37f043639b
frepr183@student.liu.se <frepr183@student.liu.se>
2016-10-13 06:56:59 +0200

    Added further documentation to the ray class. Signed-off-by: frepr183@student.liu.se <frepr183@student.liu.se>

--- src/Geometry/Ray.cpp
+++ src/Geometry/Ray.cpp
@@ -1,13 +1,12 @@
 #include "Ray.h"
 
+#include <iostream>
+
 Ray::Ray(glm::vec3 _from, glm::vec3 _dir) :
 	from(_from), direction(_dir) {}
 
-Ray::Ray() {
-
-}
+Ray::Ray() { }
 
 void Ray::Update() {
-
-
+	std::cerr << "Ray AABB update has not been implemented yet (Ray.cpp)." << std::endl;
 }
--- src/Geometry/Ray.h
+++ src/Geometry/Ray.h
@@ -2,17 +2,13 @@
 
 #include <glm.hpp>
 
-/// <summary> Describes a 3D ray. </summary>
+/// <summary> 
+/// Describes a 3D ray. 
+/// Ray is parametrized as X = F + t * D (where F = from and D = direction).
+/// </summary>
 class Ray {
 public:
-	// Ray is parametrized as X = F + t * D (where F = from and D = direction).
 	glm::vec3 direction, from;
-
-	/* Extra data needed for fast AAABB intersection testing. */
-	float idirx, idiry, idirz; // TODO: Change name of these to something more intuitive.
-	float cxy, cxz, cyx, cyz, czx, czy; // TODO: Change name of these to something more intuitive.
-	float ibyj, jbyi, jbyk, kbyj, ibyk, kbyi; // TODO: Change name of these to something more intuitive.
-
 	Ray(glm::vec3 from, glm::vec3 direction);
 	Ray();
 
@@ -20,4 +16,12 @@ public:
 	/// Updates ray AABB intersection testing data. This should be called whenever the ray is altered.
 	/// </summary>
 	void Update();
+
+	// ------------------------------------------------------
+	// Extra data needed for fast AAABB intersection testing.
+	// ------------------------------------------------------
+	float idirx, idiry, idirz; // TODO: Change name of these to something more intuitive.
+	float cxy, cxz, cyx, cyz, czx, czy; // TODO: Change name of these to something more intuitive.
+	float ibyj, jbyi, jbyk, kbyj, ibyk, kbyi; // TODO: Change name of these to something more intuitive.
+	// ------------------------------------------------------
 };
