dd92354c92860d71a9ac0b64585dda9f74db5e95
frepr183@student.liu.se <frepr183@student.liu.se>
2016-10-13 18:22:06 +0200

    Adjusted assert in Math.cpp. Signed-off-by: frepr183@student.liu.se <frepr183@student.liu.se>

--- src/Utility/Math.cpp
+++ src/Utility/Math.cpp
@@ -12,8 +12,8 @@ using namespace std;
 float Utility::Math::BilinearInterpolation(const float dy, const float dz,
 										   const float x1, const float x2,
 										   const float x3, const float x4) {
-	assert(dy >= 0 && dy <= 1 + FLT_EPSILON);
-	assert(dz >= 0 && dz <= 1 + FLT_EPSILON);
+	assert(dy >= -FLT_EPSILON && dy <= 1 + FLT_EPSILON);
+	assert(dz >= -FLT_EPSILON && dz <= 1 + FLT_EPSILON);
 	const float idy = 1 - dy;
 	const float idz = 1 - dz;
 	float a1 = idy * dz; // lower right area.
