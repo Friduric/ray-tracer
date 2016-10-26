b3cb9657ccbf56d9451afde55e8c54b41c9f70ba
davli538 <davli538@student.liu.se>
2016-10-26 15:14:17 +0200

    fixed half vector in schlick
    Signed-off-by: davli538 <davli538@student.liu.se>

--- src/Utility/Rendering.cpp
+++ src/Utility/Rendering.cpp
@@ -3,6 +3,7 @@
 float Utility::Rendering::CalculateSchlicksApproximation(const glm::vec3 & incomingDirection, const glm::vec3 & normal, float n1, float n2) {
 	// See https://en.wikipedia.org/wiki/Schlick%27s_approximation for more information.
 	float R0 = glm::pow((n1 - n2) / (n1 + n2), 2.0f);
-	float alpha = glm::dot(normal, incomingDirection); // Should be the angle of the halfway vector ("from origin").
+	glm::vec3 halfVector = glm::normalize(glm::reflect(incomingDirection, normal) + -incomingDirection);
+	float alpha = glm::dot(normal, halfVector); 
 	return R0 + (1 - R0) * glm::pow((1 - alpha), 5.0f);
 }
